dce841b710b576cab8408f79f13ff351e0f2340b
arha <>
2018-01-23 05:25:22 +0000

    better management of modules; unicorn hat module

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 musq
 ====
 
-Run scripts and toggle GPIO on Raspberry PI (clones, and various SBC boards) by MQTT subscriptions. You can either subscribe shell scripts to topics or write python3 modules using your favourite libs.
+Run scripts and toggle GPIO on Raspberry PI (clones, and various SBC boards) by MQTT subscriptions. You can either subscribe shell scripts to topics or write python3 modules using your favourite libs.o
+
+musq is mqtt glue.
 
 
 What's supported?
@@ -10,6 +12,11 @@ What's supported?
 The following have been tested:
 * Vanilla x64 debian9
 * Raspberry Pi Zero
+
+Hats and modules supported
+--------------------------
+
+* Pimoroni Unicorn HAT (Standard, 8x8) - GPIO
 
 Planned:
 --------
---
+++ musq_modules/unicorn.py
@@ -0,0 +1,55 @@
+from musq_modules import abstract
+import logging
+#!/usr/bin/env python
+import time
+import unicornhat as unicorn
+
+class mm_unicorn(abstract.mm_abstract):
+    def __init__(self):
+        prefix="unicorn"
+        unicorn.set_layout(unicorn.AUTO)
+        unicorn.rotation(0)
+        unicorn.brightness(1)
+        width,height=unicorn.get_shape()
+
+    def call(self, topic, trigger_topic, message, config_line): 
+        logging.debug("topic=" + topic)
+        logging.debug("trigger_topic=" + trigger_topic)
+        logging.debug("message=" + message.payload.decode('UTF-8'))
+        logging.debug("config_line=" + config_line)
+        
+        topic_p=trigger_topic.split('/')
+        if (len(topic_p) < 4):
+            logging.debug("Invalid unicorn topic: " + trigger_topic)
+        else:
+            m=message.payload.decode('UTF-8')
+            logging.debug("unicorn...")
+
+            if (topic_p[len(topic_p)-1] == 'all'):
+                if (m[0] == '#'):
+                    r = int(m[1:3], 16)
+                    g = int(m[3:5], 16)
+                    b = int(m[5:7], 16)
+                else:
+                    (r,g,b) = m.split(' ')
+                    r = int(r)
+                    b = int(b)
+                    g = int(g)
+                if (r != None and b != None and g != None):
+                    for x in range(0,8):
+                        for y in range (0,8):
+                            unicorn.set_pixel(x,y,r,g,b)
+                unicorn.show()
+            if (topic_p[len(topic_p)-1] == 'led'):
+                (x,y,r,g,b) = m.split(' ')
+                x = int(x)
+                y = int(y)
+                r = int(r)
+                b = int(b)
+                g = int(g)
+                if (x!= None and y != None and r != None and b != None and g != None):
+                    unicorn.set_pixel(x,y,r,g,b)
+                    unicorn.show()
+
+    def link(self):
+        logging.debug("unicorn linked!")
