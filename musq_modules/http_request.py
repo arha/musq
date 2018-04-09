3addab5b8be09405439a4ed7801f0f57a089e750
arha <>
2018-04-09 15:26:13 +0300

    http response can now be published to a different topic

--- musq_modules/http_request.py
+++ musq_modules/http_request.py
@@ -1,6 +1,11 @@
 from musq_modules import abstract
 import logging
-from urllib.request import urlopen
+try:
+    from urllib.parse import urlencode
+    from urllib.request import urlopen
+except ImportError: # Python 2
+    from urllib import urlencode
+    from urllib2 import urlopen
 
 class mm_http_request(abstract.mm_abstract):
     def __init__(self):
@@ -13,14 +18,21 @@ class mm_http_request(abstract.mm_abstract):
         logging.debug("config_line=" + config_line)
         logging.debug("id = %s", self.my_id)
 
-
-
         if (self.settings.get('url') != None):
             url = self.settings.get('url')
             if url[0:7] != "http://":
                 url = "http://" + url
+            logging.info("Sending http request %s" % url)
             f = urlopen( url )
-            print ( f.read() )
+            result = ( f.read() )
+            print ( result )
+
+            publish_result = self.settings.get('publish_result')
+            if publish_result != None:
+                logging.info("Publishing result to %s" % publish_result)
+                publish_qos = self.settings.get('publish_qos') or "0"
+                publish_retain = self.settings.get("publish_retain") or False
+                self.creator.raw_publish(self, result, publish_result, publish_qos, publish_retain )
 
     def link(self, creator, settings):
         super(  mm_http_request, self).link(creator, settings)
