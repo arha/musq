70c2031ed1a7d4141db6e8c9316369b460521016
arha <4208594+arha@users.noreply.github.com>
2018-04-14 00:38:24 +0300

    update to v0.3

--- platforms/platform_rpi.py
+++ platforms/platform_rpi.py
@@ -82,51 +82,6 @@ class platform_rpi(platform_linux.platform_linux):
     def hat_setup(self):
         logging.debug("Setting up hat %s" % self.hat_name)
 
-    def get_all_if_data(self):
-        nic = []
-        for ix in socket.if_nameindex():
-            name = ix[1]
-            record = self.get_data_for_if(name
-            if (record != None):
-                nic.append(record)
-        return (nic)
-
-    def get_all_ips(self):
-        # TODO: /sys/class/net/<iface>/address
-        nic = []
-
-        for ix in socket.if_nameindex():
-            name = ix[1]
-            result = self.get_data_for_if( name )
-            if (result != None and result.get('ip') != None):
-                ip = result['ip']
-                if (ip[0:4] != "127."):
-                    nic.append( ip )
-
-        return (', '.join(nic))
-
-    def get_data_for_if( self, ifname):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        try:
-            info = fcntl.ioctl(
-                s.fileno(),
-                0x8915,  # SIOCGIFADDR
-                struct.pack('256s', ifname[:15].encode("UTF-8"))
-            )
-            ip = socket.inet_ntoa(info[20:24])
-
-            info = fcntl.ioctl(
-                s.fileno(),
-                0x8927,
-                struct.pack('256s', ifname[:15].encode("UTF-8"))
-            )
-            mac = ':'.join(['%02x' % (char) for char in info[18:24]])
-            result = {"name": ifname, "ip": ip, "mac": mac}
-        except OSError:
-            logging.warning("Cannot get ip/mac of interface %s, error %s: %s" % (ifname, sys.exc_info()[0], sys.exc_info()[1]))
-            result = None
-        return result
-
     def generate_musq_id(self):
         # used as a unique identifier in /musq/[id] and /musq/heartbeat/[id] so a device can be reliably identified in the mqtt topic tree
         # combines the following strings hostname : user supplied string + platform name  + cpu_serial_number + list_of_eth_macs_concatenated
